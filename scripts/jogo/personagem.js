7639ea5841247137cfda5ef7076e95c1e4d0e02e
Daniel Anderson <danielandersondasilva@gmail.com>
2020-06-24 15:20:32 -0300

    aula03

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,24 +1,30 @@
 class Personagem extends Animacao {
-    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
-        super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
-
-        this.yInicial = height - this.altura
+    constructor(matriz, imagem, x,variacaoY, largura, altura, larguraSprite, alturaSprite) {
+        super(matriz, imagem, x,variacaoY, largura, altura, larguraSprite, alturaSprite);
+        this.variacaoY = variacaoY;
+        this.yInicial = height - this.altura - 30
         this.y = this.yInicial
-
+        
         this.velocidadeDoPulo = 0
-        this.gravidade = 3
+        this.gravidade = 6;
+        this.alturaDoPulo = -50;
+        this.pulos = 0;
     }
-
+    
     pula() {
-        this.velocidadeDoPulo = - 30
+        if (this.pulos < 2) {
+            this.velocidadeDoPulo = this.alturaDoPulo;
+            this.pulos++;
+        }
     }
-
+    
     aplicaGravidade() {
         this.y = this.y + this.velocidadeDoPulo
         this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade
-
+        
         if (this.y > this.yInicial) {
             this.y = this.yInicial
+            this.pulos = 0;
         }
     }
 
--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
 let imagemCenario;
 let imagemPersonagem;
 let imagemInimigo;
+let imagemInimigoGrande;
+let imagemInimigoVoador;
+let imagemGameOver;
 let somDoPulo
 let cenario;
 let somDoJogo
 let personagem;
 let inimigo;
+let inimigoGrande;
+let inimigoVoador;
+let pontuacao;
 
 const matrizInimigo = [[]];
 for (let i = 0; i < 7; i++) {
@@ -26,25 +32,57 @@ for (let i = 0; i < 4; i++) {
 
   }
 }
+const matrizInimigoGrande = [[]];
+for (let i = 0; i < 6; i++) {
+  for (let j = 0; j < 5; j++) {
+    matrizInimigoGrande.push([j * 400, i * 400])
+    // console.log(j * 400, i * 400);
+    if (j === 2 && i === 5) {
+      break;
+    }
+
+  }
+}
+const matrizInimigoVoador = [[]];
+for (let i = 0; i < 6; i++) {
+  for (let j = 0; j < 3; j++) {
+    matrizInimigoVoador.push([j * 200, i * 150])
+    // console.log(j * 200,j, i * 150,i);
+    if (j === 0 && i === 5) {
+      break;
+    }
+
+  }
+}
+
+const inimigos = [];
 
 function preload() {
   imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemGameOver = loadImage('imagens/assets/game-over.png');
   imagemPersonagem = loadImage('imagens/personagem/correndo.png');
-  imagemInimigo =
-    loadImage('imagens/inimigos/gotinha.png')
-  somDoJogo =
-    loadSound('sons/trilha_jogo.mp3');
-  somDoPulo =
-    loadSound('sons/somPulo.mp3');
+  imagemInimigo = loadImage('imagens/inimigos/gotinha.png')
+  imagemInimigoGrande = loadImage('imagens/inimigos/troll.png')
+  imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png')
+  somDoJogo = loadSound('sons/trilha_jogo.mp3');
+  somDoPulo = loadSound('sons/somPulo.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
-  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270);
-  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52, 52, 104, 104);
-  frameRate(30)
-  somDoJogo.loop();
+  pontuacao = new Pontuacao();
+  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 30, 110, 135, 220, 270);
+  const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 30, 52, 52, 104, 104, 10, 2500);
+  const inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width - 52, 0, 200, 200, 400, 400, 10, 500);
+  const inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width - 52, 400, 100, 75, 200, 150, 100, 3000);
+
+  inimigos.push(inimigo);
+  inimigos.push(inimigoGrande);
+  inimigos.push(inimigoVoador);
+
+  frameRate(40)
+  //somDoJogo.loop();
 }
 
 function keyPressed() {
@@ -58,15 +96,20 @@ function draw() {
   cenario.exibe();
   cenario.move();
 
+  pontuacao.exibe();
+  pontuacao.adicionarPonto();
+
   personagem.exibe();
   personagem.aplicaGravidade();
 
-  inimigo.exibe();
-  inimigo.move();
+  inimigos.forEach(inimigo => {
+    inimigo.exibe();
+    inimigo.move();
+    if (personagem.estaColidindo(inimigo)) {
+      image(imagemGameOver, width/2 - 200, height/2)
+      console.log('Colidiu')
+      noLoop()
+    }
 
-  if (personagem.estaColidindo(inimigo)) {
-    console.log('Colidiu')
-    noLoop()
-  }
-
+  })
 }
